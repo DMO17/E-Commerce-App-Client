67b8444b912d2775b23408d659cc43191666afa3
Dahir Mohamed <dmoabdi98@gmail.com>
2022-07-24 00:48:26 +0100

    product card and list designed and done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,8 @@
 import { Announcement } from "../components/Announcement";
+import { Categories } from "../components/Categories";
 import { NavBar } from "../components/NavBar";
+import { Newsletter } from "../components/Newsletter";
+import { Products } from "../components/Products";
 import Slider from "../components/Slider";
 
 export const Home = () => {
@@ -8,6 +11,9 @@ export const Home = () => {
       <Announcement />
       <NavBar />
       <Slider />
+      <Categories />
+      <Products />
+      <Newsletter />
     </div>
   );
 };
