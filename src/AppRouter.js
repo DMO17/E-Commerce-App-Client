f9e9335932ec0a97a9962d379a1dcc7314445c9c
Dahir Mohamed <dmoabdi98@gmail.com>
2022-07-23 23:41:21 +0100

    CREATED NABAR

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Pay } from "./components/Pay";
 import { Success } from "./components/Success";
+import { Home } from "./pages/Home";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/pay" element={<Pay />} />;
       <Route path="/success" element={<Success />} />;
+      <Route path="/" element={<Home />} />;
     </Routes>
   );
 };
